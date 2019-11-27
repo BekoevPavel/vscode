c147fe4f638c0183086bd5bdc4febd72e48ec22a
Pavel Bekoev <pablank@bk.ru>
2019-11-28 00:05:06 +0300

    ff

--- web_project/urls.py
+++ web_project/urls.py
@@ -8,7 +8,7 @@ import blog
 import home
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    # url(r'^landing/', include('landing.urls')),
+     url(r'^landing/', include('landing.urls')),
      url(r'^blog/', include('blog.urls')),
      url(r'', include('home.urls')),
 ]
